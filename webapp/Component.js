7f698917d0998e3bccbc29cc5184a0202390cb39
Oleksandr Yagovkin <oleksandr_yagovkin@epam.com>
2019-06-24 08:41:25 +0000

    Comit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ZCLP_HEADER.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ZCLP_HEADER.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
